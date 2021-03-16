b72e5aceb2e60bc2c873df705c62c9d5c0824ae1
sandip-android-developer <sksandipkumar588@gmail.com>
2021-03-17 01:00:57 +0530

    Image from camera and gallary Functionalities added commit 16-03-21

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_app/screens/profile/CardView.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -8,9 +11,13 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreen extends State<ProfileScreen> {
+  File _image;
+  final _picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Profile Screen"),
@@ -23,21 +30,41 @@ class _ProfileScreen extends State<ProfileScreen> {
             ConstrainedBox(
               constraints: BoxConstraints.tight(Size(double.infinity, 140.0)),
               //  padding: const EdgeInsets.fromLTRB(20.0, 60.0, 20.0, 40.0),
-              child: Stack(
-                  alignment: Alignment.center,
-                  children: [
+              child: Stack(alignment: Alignment.center, children: [
                 new CircleAvatar(
                   radius: 140.0,
                   backgroundColor: Colors.white,
-                  backgroundImage: AssetImage('assets/images/profile_scott.png'),
-                  ),
-                new CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 12.0,
-                  child: Icon(
-                    Icons.camera_alt,
-                    size: 15.0,
-                    color: Color(0xFF404040),
+                  backgroundImage:
+                      AssetImage('assets/images/profile_scott.png'),
+                ),
+                new GestureDetector(
+                  onTap: () {
+                    _showPicker(context);
+                  },
+                  child: CircleAvatar(
+                    radius: 55,
+                    backgroundColor: Color(0xffFDCF09),
+                    child: _image != null
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Image.file(
+                              _image,
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.fitHeight,
+                            ),
+                          )
+                        : Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.circular(50)),
+                            width: 100,
+                            height: 100,
+                            child: Icon(
+                              Icons.camera_alt,
+                              color: Colors.grey[800],
+                            ),
+                          ),
                   ),
                 )
               ]),
@@ -104,5 +131,60 @@ class _ProfileScreen extends State<ProfileScreen> {
         ),
       ),
     );
+  }
+
+  void _showPicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                    leading: new Icon(Icons.photo_library),
+                    title: new Text('Photo Library'),
+                    onTap: () {
+                      _impFromGallary();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      _imageFromcamera();
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  _impFromGallary() async {
+    PickedFile image =
+        await _picker.getImage(source: ImageSource.gallery, imageQuality: 50);
+    setState(() {
+      if (_image != null) {
+        _image = File(image.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  void _imageFromcamera() async {
+    PickedFile image =
+        await _picker.getImage(source: ImageSource.camera, imageQuality: 50);
+    setState(() {
+      if (_image != null) {
+        _image = File(image.path);
+      } else {
+        print('No image selected.');
+      }
+    });
   }
 }
